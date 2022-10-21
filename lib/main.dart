19c593e1be5e99c35ce6a4095bd17eafba580a6a
bhoslan <bhoslan@gmail.com>
2022-10-21 11:46:04 +0300

    21.2 ekim

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mat_lgs/view/login/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const HomePage());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const HomePage());
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
